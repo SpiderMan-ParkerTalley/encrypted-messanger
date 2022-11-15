923af907f37a6b31523ec2b805578cfb13572844
SpiderMan-ParkerTalley <ptmn9278@gmail.com>
2022-11-14 19:02:08 -0600

    Added encryption and outputs to console to explain what is currently occuring

--- client.py
+++ client.py
@@ -1,15 +1,30 @@
+from cryptography.fernet import Fernet
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("STARTING CLIENT...")
 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 12345))
+print("CONNECTED TO SERVER...")
+
+print("WAITING FOR KEY...")
+key = client.recv(1024)
+print("KEY RECIEVED: " + str(key))
+
+cipher = Fernet(key)
 
 done = False
 
 while not done:
-    client.send(input("Message: ").encode('utf-8'))
-    msg = client .recv(1024).decode('utf-8')
-    if msg == 'quit':
+    send = input("Message: ")
+    if send == 'quit':
+        client.send(cipher.encrypt(bytes(send, 'utf-8')))
         done = True
     else:
-        print(msg)
+        client.send(cipher.encrypt(bytes(send, 'utf-8')))
+        recieved = cipher.decrypt(client.recv(1024)).decode('utf-8')
+        if recieved == 'quit':
+            print("QUIT RESPONSE RECEIVED FROM SERVER, CLOSING MESSANGER")
+            done = True
+        else:
+            print(recieved)
